aaf7441ae4f0ed36430cb726712cd87e6a9359a0
auto <784819492@qq.com>
2019-03-02 14:26:02 +0800

    用户列表的bug修改

--- src/js/user-defined.js
+++ src/js/user-defined.js
@@ -13,16 +13,16 @@ layui.config({
         layer.open({
             title: false,
             type: 1,
-            content: '	<div class="admin-header-lock" id="lock-box" style="text-align:center; width:300px;height:200px;margin-top:30px;border-radius:10px">' +
+            content: '	<div class="admin-header-lock" id="lock-box" style="text-align:center; width:350px;height:200px;margin-top:30px;border-radius:10px">' +
                 '<div class="admin-header-lock-img"><img src="images/xxx.jpg" class="layui-nav-img"></div>' +
                 '<div class="admin-header-lock-name" id="lockUserName" style="margin-top:10px">Administartor</div>' +
                 '<div class="input_btn">' +
-                '<input type="password" class="admin-header-lock-input layui-input" autocomplete="off" placeholder="请输入密码解锁.." name="lockPwd" id="lockPwd" style="width:200px;margin:10px auto;"/>'
+                '<input type="password" class="admin-header-lock-input layui-input" autocomplete="off" placeholder="请输入密码解锁.." name="lockPwd" id="lockPwd" style="width:250px;margin:10px auto;"/>'
                 // '<input type="password" name="lockPwd" id="lockPwd" placeholder="请输入密码解锁.." autocomplete="off" class= "form-control" style="width:200px"/>'
                 +
                 '<button class="layui-btn" id="unlock" style="margin-top:10px">解锁</button>' +
                 '</div>' +
-                '<p style="margin-top:10px">请输入“520”，否则不会解锁成功哦！！！</p>' +
+                '<p style="margin-top:10px">请输入“520”，否则不会解锁成功哦(功能还在完善)！！！</p>' +
                 '</div>',
             closeBtn: 0,
             shade: 0.9
